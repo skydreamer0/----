07eb2ca52edae9c54897597c6258764a17668066
GeorgeHsiao <skyaski00@gmail.com>
2024-11-29 20:35:47 +0800

    目前錯誤的部分

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
