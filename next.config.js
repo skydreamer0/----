3cc373dac07b23d6dbb68a18d820a671bdd921f6
GeorgeHsiao <skyaski00@gmail.com>
2024-11-30 12:00:54 +0800

    update deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'http://twbeauty.us.kg' : '',
   images: {
     unoptimized: true,
+    domains: ['images.unsplash.com'],
   },
   trailingSlash: true,
 }
