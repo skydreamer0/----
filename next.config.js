4cc6d3d8141690fab16cfb92f69b2c962d2ff558
GeorgeHsiao <skyaski00@gmail.com>
2024-11-30 12:37:40 +0800

    setting next to use http   s

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'http://twbeauty.us.kg' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://twbeauty.us.kg' : '',
   images: {
     unoptimized: true,
     domains: ['images.unsplash.com'],
